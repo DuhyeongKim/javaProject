04c352bb8082f3948b28c3e5de2ebe4a623179a4
DuhyeongKim <49421181+DuhyeongKim@users.noreply.github.com>
2019-04-27 20:50:15 +0900

    Update JongGangCalculator.java

--- JongGangCalculator.java
+++ JongGangCalculator.java
@@ -32,6 +32,11 @@ public class JongGangCalculator{
  * this method return the due date until the end of this semester
  * and make the output of the due date.
  */ 
+    public viod setter(int currentDate){
+        this.currentDate = currentDate;
+        
+    
+    }
 
     
     public int countDays(int jgDate, int curDate){
